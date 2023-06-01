c236e86939bcb0bc0079b47c09d3329fcc7b01de
Vitor Filetto <36848688+vitorfltt@users.noreply.github.com>
2023-06-01 13:34:57 -0300

    change metrics

--- scenarios/Get-Customer.js
+++ scenarios/Get-Customer.js
@@ -1,29 +1,22 @@
 import http from "k6/http";
 import { sleep } from "k6";
-import { Rate, Trend } from "k6/metrics";
+import { Rate } from "k6/metrics";
 
 export let successRate = new Rate("request_success_rate");
-export let requestDuration = new Trend("request_duration");
 
 export let options = {
   vus: 100,
   duration: "1m",
-
   thresholds: {
     request_success_rate: ["rate>=0.8"],
-
-    request_duration: ["p(95)<300000"],
+    http_req_duration: ["p(95)<300000"],
   },
 };
 
 export default function () {
-  let start = new Date().getTime();
   let res = http.get("https://reqres.in/api/users");
-  let end = new Date().getTime();
-  let duration = end - start;
 
   successRate.add(res.status === 200);
-  requestDuration.add(duration);
 
   sleep(1);
 }
